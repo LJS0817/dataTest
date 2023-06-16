24bfabbc29255fa41cd2c75d04b68174fa304984
LJS0817 <57212058+LJS0817@users.noreply.github.com>
2023-06-16 11:27:36 +0900

    Create Type.java

---
+++ Type.java
@@ -0,0 +1,39 @@
+
+public enum DATA_TYPE {
+    E_NAME,
+    E_DATE,
+    E_ADD,
+    E_TYPE,
+
+    E_LYE_COUNT,
+    E_LYE_PURITY,
+    E_WATER_TEXT,
+    E_TOTAL_WEIGHT,
+
+    E_WEIGHT,
+    E_OIL,
+    E_LYE,
+    E_WATER,
+
+    E_SAT,
+    E_UNSAT,
+
+    E_S0,
+    E_S1,
+    E_S2,
+    E_S3,
+
+    E_U0,
+    E_U1,
+    E_U2,
+    E_U3,
+    E_U4,
+
+    E_PURE,
+    E_SOLVENT,
+    E_ETHANOL,
+    E_GLYCERINE,
+    E_SUGAR,
+    E_MEMO,
+    E_LENGTH;
+}
